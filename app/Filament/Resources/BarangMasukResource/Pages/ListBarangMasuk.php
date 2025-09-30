c2061f4742c9ef046cb52c9378f7f987c167665d
panca <ykzaditia5@gmail.com>
2025-09-30 17:55:38 +0700

    feat: Menambahkan fitur Print pada List Barang Masuk dan perbaikan fungsionalitas tabel

--- app/Filament/Resources/BarangMasukResource/Pages/ListBarangMasuk.php
+++ app/Filament/Resources/BarangMasukResource/Pages/ListBarangMasuk.php
@@ -6,11 +6,21 @@ use App\Filament\Resources\BarangMasukResource;
 use Filament\Resources\Pages\ListRecords;
 use Filament\Tables;
 use Filament\Tables\Actions\DeleteAction;
+use Filament\Actions\CreateAction; // Import CreateAction
 
 class ListBarangMasuk extends ListRecords
 {
     protected static string $resource = BarangMasukResource::class;
 
+    // Menambahkan tombol Tambah Barang Masuk ke header halaman
+    protected function getHeaderActions(): array
+    {
+        return [
+            CreateAction::make(),
+        ];
+    }
+
+    // Tombol di setiap baris tabel (Edit dan Delete)
     protected function getTableActions(): array
     {
         return [
